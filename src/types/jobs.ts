7a5e164261e9b59f5682ee70319f5cf89951ec74
florianjuesten <flojuesten@gmail.com>
2020-10-21 11:24:59 +0200

    added daily tasks

--- src/types/jobs.ts
+++ src/types/jobs.ts
@@ -1,6 +1,13 @@
 import { CreateCardJob } from './create.card.job'
 
 export interface Jobs {
+  createMonday:CreateCardJob[]
+  createTuesday:CreateCardJob[]
+  createWednesDay:CreateCardJob[]
+  createThursday:CreateCardJob[]
+  createFriday:CreateCardJob[]
+  createSaturday:CreateCardJob[]
+  createSunday:CreateCardJob[]
   createMonthly: CreateCardJob[]
   createWeekly: CreateCardJob[]
   createWeeklyEven: CreateCardJob[]
